11fb50bfd9356ec01ead5b4eeae07ad136fb0158
Vitor Saraiva de Lima <vitorsaraivadelima@hotmail.com>
2023-10-17 11:58:13 -0300

    Inicio da implementação base da página de admin

--- uberdeonibusapp/admin.py
+++ uberdeonibusapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from uberdeonibusapp.models import Person, Course, Grade
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    pass
--- uberdeonibusapp/models.py
+++ uberdeonibusapp/models.py
@@ -1,3 +1,26 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    last_name = models.TextField()
+    first_name = models.TextField()
+    courses = models.ManyToManyField("Course", blank=True)
+
+    class Meta:
+        verbose_name_plural = "People"
+
+
+class Course(models.Model):
+    name = models.TextField()
+    year = models.IntegerField()
+
+    class Meta:
+        unique_together = ("name", "year", )
+
+
+class Grade(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    grade = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)])
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
