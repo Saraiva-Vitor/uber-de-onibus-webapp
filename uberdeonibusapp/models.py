345ae83ff2087ec0dcdda2c10af1dfc231185f6d
Vitor Saraiva de Lima <vitorsaraivadelima@hotmail.com>
2023-10-19 20:50:35 -0300

    Populando Banco de dados
    Co-authored-by: Mateus Lima <immlima@users.noreply.github.com>
    Co-authored-by: JotaveLima <JotaveLima@users.noreply.github.com>

--- uberdeonibusapp/models.py
+++ uberdeonibusapp/models.py
@@ -4,12 +4,15 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=20, decimal_places=15)
+    longitude = models.DecimalField(max_digits=20, decimal_places=15)
 
     def __str__(self):
         return self.name
 
+# https://www.google.com.br/maps/@-22.2838797,-42.5370439,16z?entry=ttu
+# https://www.google.com.br/maps/@-22.2879963,-42.5354955
+# https://www.google.com.br/maps/dir/-22.2878891,-42.5329371/-22.2835792,-42.5311349/data=!4m2!4m1!3e0?entry=ttu
 
 class BusRoute(models.Model):
     name = models.CharField(max_length=100)
