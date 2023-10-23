90dbba84492e527012db18722e65d2ba368c7fef
Vitor Saraiva de Lima <vitorsaraivadelima@hotmail.com>
2023-10-23 02:14:37 -0300

    Início implementação back-end menu de destinos

--- uberdeonibusapp/views.py
+++ uberdeonibusapp/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from .models import Location, BusRoute
 
 # Create your views here.
-
-
-
 def home(request):
-    return render(request, 'home.html')
+    locations = Location.objects.all()
+    routes = BusRoute.objects.all()
+    selected_destination = None
+
+    if request.method == 'POST':
+        selected_origin_id = request.POST.get('origin')
+        selected_destination_id = request.POST.get('destination')
+
+        # Se a origem e o destino forem selecionados, obtenha o destino selecionado
+        if selected_origin_id and selected_destination_id:
+            selected_destination = Location.objects.get(
+                id=selected_destination_id)
+
+    return render(request, 'home.html', {'routes': routes, 'locations': locations, 'selected_destination': selected_destination})
 
 
 def login(request):
