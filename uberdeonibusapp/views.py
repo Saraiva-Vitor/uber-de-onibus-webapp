f42be33e4d68d8c4bd0a5005f57194f4651c8d04
Vitor Saraiva de Lima <vitorsaraivadelima@hotmail.com>
2023-10-29 16:28:01 -0300

    correção possiveis destinos

--- uberdeonibusapp/views.py
+++ uberdeonibusapp/views.py
@@ -2,37 +2,16 @@ from django.shortcuts import render
 from .models import Location, BusRoute
 
 # Create your views here.
-# def home(request):
-#     locations = Location.objects.all()
-#     routes = BusRoute.objects.all()
-
-#     if request.method == 'POST':
-#         valor1 = request.POST.get('origin.id')
-#         valor2 = request.POST.get('destination.id')
-
-#         resultados = BusRoute.objects.filter(origin=valor1, destination=valor2)
-
-#         return render(request, 'pesquisa.html', {'resultados': resultados})
-
-#     return render(request, 'home.html', {'routes': routes, 'locations': locations})
-
-
 def login(request):
     return render(request, 'login.html')
 
 def cadastro(request):
     return render(request, 'cadastro.html')
 
-# def pesquisa(request):
-#     locations = Location.objects.all()
-#     routes = BusRoute.objects.all()
-    
-#     return render(request, 'pesquisa.html', {'routes': routes, 'locations': locations})
-
-
 def home(request):
     locais = Location.objects.all()
-    return render(request, 'home.html', {'locais': locais})
+    routes = BusRoute.objects.all()
+    return render(request, 'home.html', {'locais': locais, 'routes': routes})
 
 def pesquisa(request):
     if request.method == 'GET':
