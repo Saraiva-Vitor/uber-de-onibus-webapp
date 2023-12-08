24128b14b3d434091848e9170fdb9488270137c4
Vitor Saraiva de Lima <vitorsaraivadelima@hotmail.com>
2023-12-08 20:04:47 -0300

    fix CSRF

--- uberdeonibus/settings.py
+++ uberdeonibus/settings.py
@@ -193,6 +193,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.csrf.middleware.CsrfMiddleware',
 ]
 
 ROOT_URLCONF = 'uberdeonibus.urls'
